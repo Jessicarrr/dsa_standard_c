ca64fd521542741a6dd7ac417e696b9d185e62ea
Jessicarrr <jwp01993@gmail.com>
2025-02-12 22:51:24 +1100

    feat: array copying in helpers.h and .c

--- src/helpers.c
+++ src/helpers.c
@@ -1,7 +1,14 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "helpers.h"
 #include <stdbool.h>
+
+typedef enum {
+    SUCCESS = 0,
+    ERROR_SIZE_MISMATCH = 1,
+    ERROR_NULL_ARRAY = 2
+} CopyStatus;
 
 bool is_in_array(int* arr, int size, int value) {
     for (int i = 0; i < size; i++) {
@@ -33,4 +40,45 @@ void swap_elements(int* arr, int left, int right) {
 
     arr[left] = arr[right];
     arr[right] = left_value;
+}
+
+int* copy_array(int* array, int size) {
+    int* new_array = malloc(sizeof(int) * size);
+
+    for(int pointer = 0; pointer < size; pointer++) {
+        new_array[pointer] = array[pointer];
+
+    }
+
+    return new_array;
+
+}
+
+int copy_array_to(int* source_array, int source_size,
+                   int* destination_array, int destination_size) {
+
+    if (source_array == NULL) {
+        fprintf(stderr, "copy_array_to could not complete because "
+                        "the source array was null.");
+        return ERROR_NULL_ARRAY;
+    }
+
+    if (destination_array == NULL) {
+        fprintf(stderr, "copy_array_to could not complete because "
+                        "the destination array was null.");
+        return ERROR_NULL_ARRAY;
+    }
+
+    if(source_size != destination_size) {
+        fprintf(stderr, "copy_array_to requires both arrays to be the "
+                        "same size.");
+        return ERROR_SIZE_MISMATCH;
+    }
+
+    for(int i = 0; i < source_size; i++) {
+        destination_array[i] = source_array[i];
+    }
+
+    return SUCCESS;
+
 }
